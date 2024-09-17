208dc2e278050f72e3d02e54c2569e9c0e9e0f56
Kaire Viana <kaireviana20gmail.com>
2024-09-16 23:10:55 -0300

    Iniciando o versionamento

---
+++ app.py
@@ -0,0 +1,8 @@
+print('Fiu da Navalha!\n')
+print('1 - Agendar horário')
+print('2 - Mostrar clientes agendados')
+print('3 - Excluir clientes agendados')
+print('4 - Finalizar programa')
+
+def agendar_horarios():
+    pass
